2fa881a8af5df481692168a8b162a2792aa2da60
Gabriel NK <86859418+gabrielnakhata@users.noreply.github.com>
2023-06-23 20:03:33 -0300

    Update App.js

--- app/dream-mapp/App.js
+++ app/dream-mapp/App.js
@@ -3,9 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Appbar, Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import ProfilePage from './src/pages/ProfilePage';
-
-
 
 import NavigationContainer from './src/components/NavigationContainer';
 import Navigation from './src/components/Navigation';
@@ -13,7 +10,6 @@ import theme from './src/components/DefaultTheme';
 
 export default function App() {
 
-<<<<<<< HEAD
   return (
     <NavigationContainer>
     <SafeAreaProvider>
@@ -24,16 +20,12 @@ export default function App() {
     </NavigationContainer>
   );
 }
-=======
-  const _goBack = () => console.log('Went back');
-  const _handleSearch = () => console.log('Searching');
-  const _handleMore = () => console.log('Shown more');
 
- 
-  return (
->>>>>>> feature/profile-page
-
-    <ProfilePage />
-  );
-};
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
